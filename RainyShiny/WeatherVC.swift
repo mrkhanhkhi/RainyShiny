431a0395a37919e07e4d944131e83d603f197f5d
Nguyen Duy Khanh <dLn-wN7-GMe-d9pkhanhnd@runsystem.net>
2017-03-17 17:26:48 +0700

    first commit

---
+++ RainyShiny/WeatherVC.swift
@@ -0,0 +1,43 @@
+//
+//  WeatherVC.swift
+//  RainyShiny
+//
+//  Created by Nguyen Duy Khanh on 3/17/17.
+//  Copyright © 2017 Nguyen Duy Khanh. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLable: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell (withIdentifier: "weatherCell", for: indexPath)
+        return cell
+    }
+    
+    
+}
+
